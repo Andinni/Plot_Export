1a8ac46dcce1e9fd5f7449b9e749fd2518668521
Andreas Inhofer <andreas.inhofer@web.de>
2017-02-13 15:24:18 +0100

    Created the MainWindow object.
    Allows the "user" do create a MainWindow object and call
    MainWindow().save_line(fig) in order to replot an existing figure. This
    code adds an additional button. If this button is clicked, txt files
    containing all x-y data from the plot is created.

---
+++ data_exp_plot.py
@@ -0,0 +1,83 @@
+import sys
+from PyQt4 import QtGui, QtCore
+import ConfigParser
+import numpy as np
+#from analyser import Analyser
+#from fit_config_Widget import CfgWidget
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
+
+
+class MainWindow(QtGui.QWidget):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.initUI()
+
+    def initUI(self): 
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))        
+        
+        self.mplvl = QtGui.QVBoxLayout() 
+        self.mplwindow = QtGui.QWidget()
+        self.data_exp_btn = QtGui.QPushButton(self)
+        self.data_exp_btn.setText('Save data')
+        self.data_exp_btn.clicked.connect(self.save_line)        
+        
+        self.mplvl.addWidget(self.data_exp_btn)
+        self.setLayout(self.mplvl)   
+        
+        self.setGeometry(500, 400, 600, 400)
+        self.setWindowTitle('Simple Plotter')   
+        self.show()
+#        x = np.linspace(0,10)
+#        y = np.linspace(0,20)
+#        f = Figure()
+#        self.ax1 = f.add_subplot(111)
+#        self.ax1.plot(x,y)
+#        self.addmpl(f)
+        
+    def addmpl(self, fig):
+        self.canvas = FigureCanvas(fig)
+        
+        self.canvas.draw()
+        self.toolbar = NavigationToolbar(self.canvas, 
+                        self.mplwindow, coordinates=True)
+        self.mplvl.addWidget(self.toolbar)    
+        self.mplvl.addWidget(self.canvas)
+        
+    def save_line(self):
+        axes = self.canvas.figure.get_axes()
+        for i1,ax in enumerate(axes):
+            for i2, line in enumerate(ax.get_lines()):
+                xy = line.get_xydata()
+                np.savetxt(str(i1)+str(i2)+'.txt',xy,delimiter = '\t')
+
+        
+
+        
+def main():
+
+
+    # Start application    
+    app = QtGui.QApplication(sys.argv)
+    ex = MainWindow()
+    ret = app.exec_()
+#    sys.exit(app.exec_())
+
+    
+if __name__ == '__main__':
+    main()
+    
+
+
+
+
+
+
+
+
+
+
+    
